52be6ecae9d96a132a9459d0735aeaa08bdcecab
Ashra <arash.ashrafnejad@gmail.com>
2024-05-09 15:25:14 +0600

    api base added

---
+++ src/utils/apibase.js
@@ -0,0 +1,10 @@
+import axios, { Axios } from 'axios';
+// Be careful when using SSR for cross-request state pollution
+// due to creating a Singleton instance here;
+// If any client changes this (global) instance, it might be a
+// good idea to move this instance creation inside of the
+// "export default () => {}" function below (which runs individually
+// for each client)
+export const api = axios.create({ baseURL: 'https://coding-activity-git-dev-c4r-io.vercel.app' });
+// export const api = axios.create({ baseURL: 'https://coding-activity.vercel.app' });
+// export const api = axios.create({ baseURL: 'http://localhost:3000' });
